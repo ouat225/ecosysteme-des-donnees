a60710de8675c567fd100b21cd23a0b73bbad371
ouat225 <ouattaramjeanmarc@gmail.com>
2026-01-30 17:49:04 +0100

    Mise a jour finale : Readme et Architecture Docker

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import math
 
 def calcul_lotka_volterra(Ni, Nj, r, K, alpha):
-    """
-    Calcule la population au temps t+1 selon la formule discrète du sujet :
-    Nt+1 = Nt * (1 + r * (1 - (Nt + alpha * N_adverse) / K))
-    """
+
     # 1. Calcul de la densité (compétition intra + inter-spécifique)
     term_densite = (Ni + (alpha * Nj)) / K
     
@@ -18,10 +15,7 @@ def calcul_lotka_volterra(Ni, Nj, r, K, alpha):
     return max(0, Ni_next)
 
 def get_simulation_nj(temps):
-    """
-    Simule la population adverse (Groupe H - Zombies) pour la phase de test.
-    Oscille entre 800 et 1200 pour voir l'impact sur la Wyverne.
-    """
+    
     base = 1000
     amplitude = 200
     # Cosinus pour faire des vagues
